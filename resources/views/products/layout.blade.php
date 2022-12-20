63476e1909a3fc1b0f47ee5fb88f5e4d2a3a60de
PesanduJ <devroot.gg@gmail.com>
2022-12-21 04:39:12 +0530

    foruth commit

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -2,8 +2,11 @@
 <html>
     <head>
         <title>Laravel 9 CRUD Application</title>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
+        <!--
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
         rel="stylesheet">
+        -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
 
     <body>
